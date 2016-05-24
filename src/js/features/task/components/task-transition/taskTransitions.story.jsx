d9cf16b26473c27d67d0eddfee4c52005485490b
Andrea Soldino <andrea.soldino@gmail.com>
2016-05-24 15:21:41 +0200

    Task transition stories

--- src/js/features/task/components/task-transition/taskTransitions.story.jsx
+++ src/js/features/task/components/task-transition/taskTransitions.story.jsx
@@ -37,4 +37,82 @@ storiesOf('Task/TaskTransitions', module)
         </Paper>
       </div>
     );
+  })
+  .add('long list', () => {
+    const props = {
+      taskId: 'dummy',
+      availableTransitions: [
+        {
+          action: 'run',
+          label: 'run',
+          backgroundColor: 'orange'
+        },
+        {
+          action: 'move',
+          label: 'move',
+          backgroundColor: 'red'
+        },
+        {
+          action: 'jump',
+          label: 'jump',
+          backgroundColor: 'olive'
+        },
+        {
+          action: 'verylong',
+          label: 'very long laasdhwefheu bel',
+          backgroundColor: 'olive'
+        },
+        {
+          action: 'one',
+          label: 'one',
+          backgroundColor: 'olive'
+        },
+        {
+          action: 'two',
+          label: 'two two two two two',
+          backgroundColor: 'olive'
+        }
+      ],
+      onButtonClick: action('transition')
+    };
+
+    return (
+      <div className="center-component">
+        <Paper style={paperStyle}>
+          <TaskTransitions {...props} />
+        </Paper>
+      </div>
+    );
+  })
+  .add('disabled', () => {
+    const props = {
+      taskId: 'dummy',
+      availableTransitions: [
+        {
+          action: 'run',
+          label: 'run',
+          backgroundColor: 'orange'
+        },
+        {
+          action: 'move',
+          label: 'move',
+          backgroundColor: 'red'
+        },
+        {
+          action: 'jump',
+          label: 'jump',
+          backgroundColor: 'olive'
+        }
+      ],
+      onButtonClick: action('transition'),
+      buttonsDisabled: true
+    };
+
+    return (
+      <div className="center-component">
+        <Paper style={paperStyle}>
+          <TaskTransitions {...props} />
+        </Paper>
+      </div>
+    );
   });
