fd500cdb4eefb8ec96b0495a14455c55f13fd0a0
Deepak Menon <deepak.menon07@gmail.com>
2016-07-05 14:27:04 +0530

    Fixed issue in password changer storybook

--- src/js/features/user/components/password-changer/storybook/passwordChanger.jsx.story.jsx
+++ src/js/features/user/components/password-changer/storybook/passwordChanger.jsx.story.jsx
@@ -1,8 +1,17 @@
 import React from 'react';
 import { storiesOf, action } from '@kadira/storybook';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+
+import { Provider } from 'react-redux';
+import { store } from '../../../../../store.js';
 import { PasswordChanger } from '../index';
 
-storiesOf('User/PasswordChanger', module)
+storiesOf('User/PasswordChanger', module).addDecorator((story) => (
+  <Provider store={store}>
+    <MuiThemeProvider muiTheme={getMuiTheme()}>{story()}</MuiThemeProvider>
+  </Provider>
+  ))
   .add('Should show the password change form', () => (
     <div className="center-component">
       <PasswordChanger
